6f5f507c2b000e3495b47027851dafd86ffb9cfc
ifb322 <ivan.zelenkov2017@yandex.ru>
2025-09-28 23:37:49 +0800

    V2

--- src/components/ui/DocsCard.tsx
+++ src/components/ui/DocsCard.tsx
@@ -7,7 +7,7 @@ interface DocsCardProps {
 export function  DocsCard({name}: DocsCardProps) {
     return(
         <div className="p-5 flex  justify-center items-center gap-5 border  rounded-2xl">
-            <h1>{name}</h1>
+             <h1>{name}</h1>
         </div>
     )
 }
